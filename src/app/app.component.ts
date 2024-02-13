4d8266a544e2e4dd32842b15bf1af4be2a709aca
Ivan <ivantolmachevmaster@gmail.com>
2024-02-13 16:52:10 +0300

    Completely remove interval component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { IntervalComponent } from './interval/interval.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, IntervalComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
