bf71c6539608e42d44c876e5c3ac1a10d13132a4
Ivan <ivantolmachevmaster@gmail.com>
2024-02-15 22:04:57 +0300

    Make list of timers in index component

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,16 +1,18 @@
+import { KeyValuePipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { TimerComponent } from '../timer/timer.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [TimerComponent],
+  imports: [TimerComponent, NgFor, KeyValuePipe],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
-  time: string = "00:00:00"
-  update_time(event: Event) {
-    this.time = (event.target as HTMLInputElement).value
+  timers: { [key: string]: string }
+    = { 'one timer': '00:00:00', 'second timer': '00:00:00' }
+  update_time(event: Event, key: string) {
+    this.timers[key] = (event.target as HTMLInputElement).value
   }
 }
