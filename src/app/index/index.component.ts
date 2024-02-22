f0693b3a6a4eafd70b9e39e9a01a301ae66e66f9
Ivan <ivantolmachevmaster@gmail.com>
2024-02-22 18:58:00 +0300

    Move timers to service

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -2,11 +2,8 @@ import { KeyValuePipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { TimerComponent } from '../timer/timer.component';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TimersService } from '../timers.service';
 
-const DEFAULT = {
-  TIMER_NAME: "default",
-  TIMER_TIME: "00:00:00"
-}
 @Component({
   selector: 'app-index',
   standalone: true,
@@ -15,21 +12,17 @@ const DEFAULT = {
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
-  timers: { [key: string]: string }
-    = { [DEFAULT.TIMER_NAME]: DEFAULT.TIMER_TIME, "asdfasdfasdfadfsasdf": DEFAULT.TIMER_TIME, "asdfasdfasdfadfsasdfasdfadsf": DEFAULT.TIMER_TIME, "asdfasdfasdfadfsasdfasdfasdfasdfasd": DEFAULT.TIMER_TIME }
+  checkoutForm
 
-  constructor(private formBuilder: FormBuilder) { }
-  checkoutForm = this.formBuilder.group({
-    name: DEFAULT.TIMER_NAME,
-    time: DEFAULT.TIMER_TIME
-  })
-  has_timer() { return Object.keys(this.timers).includes(this.checkoutForm.value.name || "") }
-  onchange(event: Event) { console.log("Change") }
-  update_time(event: Event, key: string) {
-    this.timers[key] = (event.target as HTMLInputElement).value
+  constructor(private formBuilder: FormBuilder, private timers: TimersService) {
+    const { NAME: name, TIMEOUT: time } = this.timers.DEFAULT
+    this.checkoutForm = this.formBuilder.group({ name, time })
   }
-  create_timer() {
-    this.timers[this.checkoutForm.value.name ?? "default"] = this.checkoutForm.value.time ?? '00:00:00'
-    console.log("Timer created")
+  get_timers_name() { return Object.keys(this.get_timers()) }
+  get_timers() { return this.timers.get_timers() }
+  set_timer() {
+    const { name, time: timeout } = this.checkoutForm.value
+    this.timers.set_timer(name!, timeout!)
   }
+  has_timer() { return this.timers.get_timer(this.checkoutForm.value.name!) != undefined }
 }
---
+++ src/app/timers.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimersService {
+
+  DEFAULT = {
+    NAME: "default",
+    TIMEOUT: "00:00:00"
+  }
+  private timers: { [key: string]: string } = { [this.DEFAULT.NAME]: this.DEFAULT.TIMEOUT }
+
+  public get_timers(): { [key: string]: string } { return this.timers }
+  get_timer(name: string) { return this.timers[name] }
+  set_timer(name: string, timeout: string) { this.timers[name] = timeout }
+  constructor() { }
+}
