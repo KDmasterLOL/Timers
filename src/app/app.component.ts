43a0673304533455108d33943469bb600b18bcf6
Ivan <ivantolmachevmaster@gmail.com>
2024-03-02 19:50:32 +0300

    Make dark theme and app fullscreen without layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ThemeSwitcherComponent } from './theme-switcher/theme-switcher.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, ThemeSwitcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-switcher',
+  standalone: true,
+  imports: [],
+  templateUrl: './theme-switcher.component.html',
+  styleUrl: './theme-switcher.component.scss'
+})
+export class ThemeSwitcherComponent {
+  // TODO: Implement. Get implementation from /Users/ivantolmacev/Documents/20_ProgramingProjects/23_WEB/layouts/Technology/src/app/theme-switcher
+
+}
